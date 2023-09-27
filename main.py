9fe59c81ef10e341fc56b6400743d92bec07cf52
PaulJeha <paul.jeha@gmail.com>
2023-09-27 23:02:28 +0200

    karolina enabled

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import argparse
+import pathlib
+
 import torch as t
 import torch.nn as nn
 import torchvision as tv
@@ -21,7 +24,7 @@ if dataset == "SVHN_original":
     nz, nc, ndf, ngf = 100, 3, 200, 64
     K_0, a_0, K_1, a_1 = 60, 0.4, 40, 0.1
     llhd_sigma = 0.3
-    device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
     loss_reconstruction = "gaussian"
 
 elif dataset == "SVHN":
@@ -29,7 +32,7 @@ elif dataset == "SVHN":
     nz, nc, ndf, ngf = 100, 3, 200, 64
     K_0, a_0, K_1, a_1 = 20, 0.4, 20, 0.1
     llhd_sigma = 0.3
-    device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
     loss_reconstruction = "gaussian"
 
 
@@ -38,17 +41,16 @@ elif dataset == "MNIST":
     nz, nc, ndf, ngf = 16, 1, 200, 16
     K_0, a_0, K_1, a_1 = 20, 0.4, 20, 0.1
     llhd_sigma = 0.3
-    device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
     loss_reconstruction = "bernoulli"
 
-elif dataset == "CIFAR_10" :
+elif dataset == "CIFAR_10":
     img_size, batch_size = 28, 256
     nz, nc, ndf, ngf = 16, 1, 200, 16
     K_0, a_0, K_1, a_1 = 20, 0.4, 20, 0.1
     llhd_sigma = 0.3
-    device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+    device = t.device("cuda" if t.cuda.is_available() else "cpu")
     loss_reconstruction = "gaussian"
-
 
 
 cfg = {
@@ -72,7 +74,7 @@ cfg = {
     "llhd_sigma": llhd_sigma,
     "n_iter": n_iter,
     "device": device,
-    'loss_reconstruction': loss_reconstruction,
+    "loss_reconstruction": loss_reconstruction,
 }
 
 
@@ -80,49 +82,63 @@ cfg_clipping = {
     "E_clip_grad_type": "norm",
     "E_clip_grad_value": 0.5,
     "E_nb_sigma": 3,
-
     "G_clip_grad_type": "norm",
     "G_clip_grad_value": 0.5,
     "G_nb_sigma": 3,
-
     "Encoder_clip_grad_type": "norm",
     "Encoder_clip_grad_value": 0.5,
     "Encoder_nb_sigma": 3,
 }
 
 cfg_regularization = {
-    "l2_grad":0.0,
-    "l2_param":1.0,
-    "l2_output":0.0,
+    "l2_grad": 0.0,
+    "l2_param": 1.0,
+    "l2_output": 0.0,
 }
 
 cfg_proposal = {
-    "proposal_mean" : 0.0,
-    "proposal_std" : 1.0,
+    "proposal_mean": 0.0,
+    "proposal_std": 1.0,
 }
 
 cfg.update(cfg_proposal)
 cfg.update(cfg_clipping)
 cfg.update(cfg_regularization)
 
-cfg.update({"lr_E" : 0.00005,
-            "beta1_E": 0.5,
-            "beta2_E":0.999,
-            "lr_G" : 0.0001,
-            "beta1_G": 0.5,
-            "beta2_G":0.999,
-            "lr_Encoder" : 0.0001,
-            "beta1_Encoder": 0.5,
-            "beta2_Encoder":0.999,
-            "fix_encoder" : False,
-            "fix_decoder" : False,
-})
+cfg.update(
+    {
+        "lr_E": 0.00005,
+        "beta1_E": 0.5,
+        "beta2_E": 0.999,
+        "lr_G": 0.0001,
+        "beta1_G": 0.5,
+        "beta2_G": 0.999,
+        "lr_Encoder": 0.0001,
+        "beta1_Encoder": 0.5,
+        "beta2_Encoder": 0.999,
+        "fix_encoder": False,
+        "fix_decoder": False,
+    }
+)
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A simple script with argparse")
+    parser.add_argument("-k", "--karolina", help="bool", action="store_true")
+    args = parser.parse_args()
 
-if __name__ == '__main__':
+    if args.karolina:
+        dir_name = pathlib.Path(
+            pathlib.Path.home().parent.parent, "scratch/project/dd-23-57/LEBM"
+        )
+        cfg.update({"log_dir": dir_name})
+    else:
+        cfg.update({"log_dir": None})
+
+    print(cfg)
+    raise NotImplementedError
+
     total_train = get_trainer(cfg)
-    total_train=total_train(cfg)
+    total_train = total_train(cfg)
     data_train, data_valid, data_test = get_dataset_and_loader(cfg, device)
     total_train.train(train_data=data_train, val_data=data_test)
-
--- trainers/abstract_trainer.py
+++ trainers/abstract_trainer.py
@@ -7,113 +7,162 @@ import wandb
 from log_utils import draw_samples, log
 from loss_reconstruction import get_loss_reconstruction
 from networks import network_getter
-from sampler import (sample_langevin_posterior, sample_langevin_prior,
-                     sample_p_data)
+from sampler import sample_langevin_posterior, sample_langevin_prior, sample_p_data
 
 
-class AbstractTrainer():
-    def __init__(self, cfg,) -> None:
+class AbstractTrainer:
+    def __init__(
+        self,
+        cfg,
+    ) -> None:
         self.G, self.Encoder, self.E = network_getter(cfg["dataset"], cfg)
         self.cfg = cfg
-        self.optG = torch.optim.Adam(self.G.parameters(), lr=cfg["lr_G"], betas=(cfg["beta1_G"], cfg["beta2_G"]))
-        self.optE = torch.optim.Adam(self.E.parameters(), lr=cfg["lr_E"], betas=(cfg["beta1_E"], cfg["beta2_E"]))
-        self.optEncoder = torch.optim.Adam(self.Encoder.parameters(), lr=cfg["lr_Encoder"], betas=(cfg["beta1_Encoder"], cfg["beta2_Encoder"]))
-        self.proposal = torch.distributions.normal.Normal(torch.tensor(cfg["proposal_mean"],device=cfg['device'], dtype=torch.float32),torch.tensor(cfg["proposal_std"],device=cfg['device'], dtype=torch.float32))
-        self.base_dist = torch.distributions.normal.Normal(torch.tensor(0,device=cfg['device'], dtype=torch.float32),torch.tensor(1,device=cfg['device'], dtype=torch.float32))
-        self.log_var_p = torch.tensor(0,device=cfg['device'], dtype=torch.float32)
+        self.optG = torch.optim.Adam(
+            self.G.parameters(), lr=cfg["lr_G"], betas=(cfg["beta1_G"], cfg["beta2_G"])
+        )
+        self.optE = torch.optim.Adam(
+            self.E.parameters(), lr=cfg["lr_E"], betas=(cfg["beta1_E"], cfg["beta2_E"])
+        )
+        self.optEncoder = torch.optim.Adam(
+            self.Encoder.parameters(),
+            lr=cfg["lr_Encoder"],
+            betas=(cfg["beta1_Encoder"], cfg["beta2_Encoder"]),
+        )
+        self.proposal = torch.distributions.normal.Normal(
+            torch.tensor(
+                cfg["proposal_mean"], device=cfg["device"], dtype=torch.float32
+            ),
+            torch.tensor(
+                cfg["proposal_std"], device=cfg["device"], dtype=torch.float32
+            ),
+        )
+        self.base_dist = torch.distributions.normal.Normal(
+            torch.tensor(0, device=cfg["device"], dtype=torch.float32),
+            torch.tensor(1, device=cfg["device"], dtype=torch.float32),
+        )
+        self.log_var_p = torch.tensor(0, device=cfg["device"], dtype=torch.float32)
         self.loss_reconstruction = get_loss_reconstruction(cfg)
-        self.logger = wandb.init(project="LatentEBM", config=cfg)
+
+        self.logger = wandb.init(project="LatentEBM", config=cfg, dir=cfg["log_dir"])
         self.n_iter = cfg["n_iter"]
         self.n_iter_pretrain = cfg["n_iter_pretrain"]
-        
 
     def train(self, train_data, val_data=None):
-        for i in tqdm.tqdm(range(self.n_iter_pretrain+self.n_iter)):
+        for i in tqdm.tqdm(range(self.n_iter_pretrain + self.n_iter)):
             x = sample_p_data(train_data, self.cfg["batch_size"])
             if i < self.n_iter_pretrain:
                 dic_loss = self.train_step_standard_elbo(x, i)
-            else :
+            else:
                 dic_loss = self.train_step(x, i)
             if i % self.cfg["log_every"] == 0:
                 log(i, dic_loss, logger=self.logger)
             if i % self.cfg["save_image_every"] == 0:
                 batch_save = min(64, x.shape[0])
-                z_e_0, z_g_0 = self.base_dist.sample((batch_save,self.cfg["nz"],1,1)), self.base_dist.sample((batch_save,self.cfg["nz"],1,1))
-                z_e_k = sample_langevin_prior(z_e_0, self.E, self.cfg["K_0"], self.cfg["a_0"])
-                z_g_k = sample_langevin_posterior(z_g_0, x[:batch_save], self.G, self.E, self.cfg["K_1"], self.cfg["a_1"], self.loss_reconstruction)
+                z_e_0, z_g_0 = self.base_dist.sample(
+                    (batch_save, self.cfg["nz"], 1, 1)
+                ), self.base_dist.sample((batch_save, self.cfg["nz"], 1, 1))
+                z_e_k = sample_langevin_prior(
+                    z_e_0, self.E, self.cfg["K_0"], self.cfg["a_0"]
+                )
+                z_g_k = sample_langevin_posterior(
+                    z_g_0,
+                    x[:batch_save],
+                    self.G,
+                    self.E,
+                    self.cfg["K_1"],
+                    self.cfg["a_1"],
+                    self.loss_reconstruction,
+                )
                 x_base = self.G(z_e_0)
                 x_prior = self.G(z_e_k)
                 x_posterior = self.G(z_g_k)
-                draw_samples(x_base, x_prior, x_posterior, None, i, self.logger,)
+                draw_samples(
+                    x_base,
+                    x_prior,
+                    x_posterior,
+                    None,
+                    i,
+                    self.logger,
+                )
             if i % self.cfg["val_every"] == 0 and val_data is not None:
                 self.eval(val_data, i)
-             
 
     def eval(self, val_data, step, name="val/"):
         with torch.no_grad():
-            z_e_0 = self.base_dist.sample((self.cfg["nb_sample_partition_estimate_val"],self.cfg["nz"],1,1))
+            z_e_0 = self.base_dist.sample(
+                (self.cfg["nb_sample_partition_estimate_val"], self.cfg["nz"], 1, 1)
+            )
             energy_base_dist = self.E(z_e_0).flatten(1).sum(1)
             base_dist_z_base_dist = self.base_dist.log_prob(z_e_0.flatten(1)).sum(1)
-            log_partition_estimate = torch.logsumexp(-energy_base_dist -base_dist_z_base_dist,0) - math.log(energy_base_dist.shape[0])
-            log(step, {"log_z":log_partition_estimate.item()}, logger=self.logger, name=name)
-            k=0
-            while k* self.cfg["batch_size"]<1000:
-                x = val_data[k*self.cfg["batch_size"]:(k+1)*self.cfg["batch_size"]]
+            log_partition_estimate = torch.logsumexp(
+                -energy_base_dist - base_dist_z_base_dist, 0
+            ) - math.log(energy_base_dist.shape[0])
+            log(
+                step,
+                {"log_z": log_partition_estimate.item()},
+                logger=self.logger,
+                name=name,
+            )
+            k = 0
+            while k * self.cfg["batch_size"] < 1000:
+                x = val_data[
+                    k * self.cfg["batch_size"] : (k + 1) * self.cfg["batch_size"]
+                ]
                 dic = {}
-                mu_q, log_var_q = self.Encoder(x).chunk(2,1)
-                std_q = torch.exp(0.5*log_var_q)
+                mu_q, log_var_q = self.Encoder(x).chunk(2, 1)
+                std_q = torch.exp(0.5 * log_var_q)
 
                 # Reparam trick
                 eps = torch.randn_like(mu_q)
-                z_q = (eps.mul(std_q).add_(mu_q)).reshape(-1,self.cfg["nz"],1,1)
+                z_q = (eps.mul(std_q).add_(mu_q)).reshape(-1, self.cfg["nz"], 1, 1)
                 x_hat = self.G(z_q)
-
 
                 # Reconstruction loss :
                 loss_g = self.loss_reconstruction(x_hat, x).mean(dim=0)
 
                 # KL without ebm
-                KL_loss = 0.5 * (self.log_var_p - log_var_q -1 +  (log_var_q.exp() + mu_q.pow(2))/self.log_var_p.exp())
+                KL_loss = 0.5 * (
+                    self.log_var_p
+                    - log_var_q
+                    - 1
+                    + (log_var_q.exp() + mu_q.pow(2)) / self.log_var_p.exp()
+                )
                 KL_loss = KL_loss.sum(dim=1)
 
                 # Entropy posterior
-                entropy_posterior = torch.sum(0.5* (math.log(2*math.pi) +  log_var_q + 1), dim=1)
+                entropy_posterior = torch.sum(
+                    0.5 * (math.log(2 * math.pi) + log_var_q + 1), dim=1
+                )
 
                 # Energy :
                 energy_approximate = self.E(z_q).flatten(1).sum(1)
                 base_dist_z_approximate = self.base_dist.log_prob(z_q.flatten(1)).sum(1)
 
-
-                loss_ebm = energy_approximate + log_partition_estimate.exp() -1
+                loss_ebm = energy_approximate + log_partition_estimate.exp() - 1
                 loss_total = loss_g + KL_loss + loss_ebm
 
                 dic_loss = {
-                    "loss_g":loss_g,
-                    "entropy_posterior":entropy_posterior,
+                    "loss_g": loss_g,
+                    "entropy_posterior": entropy_posterior,
                     "loss_ebm": loss_ebm,
                     "base_dist_z_approximate": base_dist_z_approximate,
-                    "base_dist_z_base_dist" : base_dist_z_base_dist,
+                    "base_dist_z_base_dist": base_dist_z_base_dist,
                     "KL_loss_no_ebm": KL_loss,
                     "energy_approximate": energy_approximate,
                     "energy_base_dist": energy_base_dist,
-                    "approx_elbo" : -loss_total,
+                    "approx_elbo": -loss_total,
                 }
                 for key, value in dic_loss.items():
-                    if key not in dic :
+                    if key not in dic:
                         dic[key] = []
                     dic[key].append(value)
-                k=k+1
+                k = k + 1
             for key in dic:
                 dic[key] = torch.stack(dic[key]).mean().item()
             log(step, dic, logger=self.logger, name=name)
-            
-            
-    
+
     def test(self, data):
         raise NotImplementedError
-
-
-    
 
     def train_step(self, x, step):
         raise NotImplementedError
@@ -124,20 +173,27 @@ class AbstractTrainer():
         self.optE.zero_grad()
         self.optEncoder.zero_grad()
 
-        z_e_0, z_g_0 = self.base_dist.sample((self.cfg["batch_size"],self.cfg['nz'],1,1)), self.base_dist.sample((self.cfg["batch_size"],self.cfg['nz'],1,1))
-        mu_q, log_var_q = self.Encoder(x).chunk(2,1)
+        z_e_0, z_g_0 = self.base_dist.sample(
+            (self.cfg["batch_size"], self.cfg["nz"], 1, 1)
+        ), self.base_dist.sample((self.cfg["batch_size"], self.cfg["nz"], 1, 1))
+        mu_q, log_var_q = self.Encoder(x).chunk(2, 1)
 
         # Reparametrization trick
-        std_q = torch.exp(0.5*log_var_q)
+        std_q = torch.exp(0.5 * log_var_q)
         eps = torch.randn_like(mu_q)
-        z_q = (eps.mul(std_q).add_(mu_q)).reshape(-1,self.cfg["nz"],1,1)
+        z_q = (eps.mul(std_q).add_(mu_q)).reshape(-1, self.cfg["nz"], 1, 1)
 
         # Reconstruction loss
         x_hat = self.G(z_q)
         loss_g = self.loss_reconstruction(x_hat, x).mean(dim=0)
 
         # KL loss
-        KL_loss = 0.5 * (log_var_p - log_var_q -1 +  (log_var_q.exp() + mu_q.pow(2))/log_var_p.exp())
+        KL_loss = 0.5 * (
+            log_var_p
+            - log_var_q
+            - 1
+            + (log_var_q.exp() + mu_q.pow(2)) / log_var_p.exp()
+        )
         KL_loss = KL_loss.sum(dim=1).mean(dim=0)
 
         # ELBO
@@ -145,8 +201,8 @@ class AbstractTrainer():
         loss_total.backward()
 
         dic_loss = {
-            "loss_g":loss_g.item(),
-            "KL_loss":KL_loss.item(),
+            "loss_g": loss_g.item(),
+            "KL_loss": KL_loss.item(),
             "elbo": -loss_total.item(),
         }
         self.optE.step()
