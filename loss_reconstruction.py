37b7f9480332e6ccb3310ab3fbe4e79e59f5100b
HugoSenetaire <hhjs@dtu.dk>
2023-09-28 17:13:25 +0200

    feat : add reconstruction

--- loss_reconstruction.py
+++ loss_reconstruction.py
@@ -1,6 +1,8 @@
+import math
+
 import torch
 import torch.nn as nn
-import math
+
 
 class LossReconstruction():
     def __init__(self) -> None:
@@ -20,6 +22,12 @@ class GaussianLossReconstruction(LossReconstruction):
         x = x.flatten(1)
         return torch.sum((x_hat-x)**2, dim=1)/ (2.0 * self.sigma * self.sigma)  - math.log(self.sigma)* x.shape[1] - 0.5 * math.log(2*math.pi) * x.shape[1]
 
+    def reconstruction(self, x_mu):
+        x = torch.normal(x_mu, self.sigma)
+        return x
+
+    
+
 class BernoulliLossReconstruction(LossReconstruction):
     def __init__(self) -> None:
         super().__init__()
@@ -31,7 +39,11 @@ class BernoulliLossReconstruction(LossReconstruction):
             print(x_hat)
             print(x)
             raise ValueError
-        return torch.sum(-x*torch.log(x_hat+1e-8) - (1-x)*torch.log(1-x_hat+1e-8), dim=1) 
+        return torch.sum(-x*torch.log(x_hat+1e-8) - (1-x)*torch.log(1-x_hat+1e-8), dim=1)
+
+    def reconstruction(self, x_mu):
+        x = torch.bernoulli(x_mu)
+        return x
     
 
 def get_loss_reconstruction(cfg):
