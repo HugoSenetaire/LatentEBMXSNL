ee7b58584a58fb3d69088cce690744233db72bdb
HugoSenetaire <hhjs@dtu.dk>
2023-10-06 10:05:20 +0200

    fix : mixture of gaussian requires cfg and samples as tuple

--- Model/Prior/mixture_of_gaussian.py
+++ Model/Prior/mixture_of_gaussian.py
@@ -4,7 +4,7 @@ import torch
 
 class MixtureOfGaussian(GaussianPrior):
     def __init__(self, cfg):
-        super().__init__()
+        super().__init__(cfg)
         self.cfg = cfg
         self.log_mix = torch.nn.Parameter(torch.randn((self.cfg.prior.nb_mixture))/self.cfg.prior.nb_mixture)
         self.mu = torch.nn.Parameter(torch.randn((self.cfg.prior.nb_mixture, self.cfg.trainer.nz)))
@@ -23,4 +23,4 @@ class MixtureOfGaussian(GaussianPrior):
         self.mix_dist = torch.distributions.categorical.Categorical(component_prob)
         self.comp_dist = torch.distributions.Independent(torch.distributions.normal.Normal(self.mu, torch.exp(self.log_var)),1)
         self.gmm = torch.distributions.mixture_same_family.MixtureSameFamily(self.mix_dist, self.comp_dist)
-        return self.gmm.sample(n)
+        return self.gmm.sample((n,))
