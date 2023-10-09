4dff71dba2187451fd6bc20c582c3bdfb3402201
HugoSenetaire <hhjs@dtu.dk>
2023-10-09 16:41:08 +0200

    feat : gaussian prior handles already the correct log prob

--- Model/Prior/gaussian_prior.py
+++ Model/Prior/gaussian_prior.py
@@ -6,13 +6,14 @@ class GaussianPrior(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.cfg = cfg
-        self.mu = nn.Parameter(torch.tensor(0.,), requires_grad=False)
-        self.std = nn.Parameter(torch.tensor(1.,), requires_grad=False)
-       
+        self.mu = nn.Parameter(torch.zeros((self.cfg.trainer.nz), device=self.cfg.trainer.device), requires_grad=False)
+        self.std = nn.Parameter(torch.ones((self.cfg.trainer.nz), device=self.cfg.trainer.device), requires_grad=False)
+              
     def log_prob(self, z):
         """By default return the gaussian 0,1"""
-        return torch.distributions.Normal(self.mu, self.std).log_prob(z)
+        return torch.distributions.Normal(self.mu, self.std).log_prob(z).reshape(z.shape[0], self.cfg.trainer.nz).sum(1)
+    
     
     def sample(self, n):
-        return torch.randn((n, self.cfg.trainer.nz, 1, 1))
+        return torch.distributions.Normal(self.mu, self.std).sample((n,))
 
